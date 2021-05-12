c655e8013488f8c7bbdd7d21736bf016d2626498
munsif12 <munsifalimisri69@gmail.com>
2021-05-13 01:13:37 +0500

    APP.js work pending

--- todo-app-react-redux/src/App.js
+++ todo-app-react-redux/src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import "./App.css";
+import { addItem } from "./redux/actions";
 
 function App() {
+  const dispatch = useDispatch();
+  const [listItem, setListItem] = useState("");
   return (
     <div className="App">
       <h1>React Redux Todo App</h1>
+      <div className="input">
+        <input
+          type="text"
+          name="item"
+          id="text"
+          value={listItem}
+          onChange={(e) => setListItem(e.target.value)}
+        />
+        <button onClick={() => dispatch(addItem(listItem))}>+</button>
+      </div>
     </div>
   );
 }
