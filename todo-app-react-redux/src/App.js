7ab73c90f3641691095c35debd9f2969f985b087
munisf12 <munsifalimisri69@gmail.com>
2021-05-14 21:36:12 +0500

    addItem_&&_deleteItem feature is completed using redux Ui is remaining

--- todo-app-react-redux/src/App.js
+++ todo-app-react-redux/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
 import "./App.css";
-import { addItem } from "./redux/actions";
+import { addItem, deleteItem } from "./redux/actions";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
 function App() {
+  const data = useSelector((state) => state.all_Reducers.list);
   const dispatch = useDispatch();
   const [listItem, setListItem] = useState("");
   return (
@@ -19,6 +20,26 @@ function App() {
           onChange={(e) => setListItem(e.target.value)}
         />
         <button onClick={() => dispatch(addItem(listItem))}>+</button>
+      </div>
+      <div className="allLists">
+        <div className="allItmes_wrapper">
+          <ul>
+            {data.map((val, index) => {
+              return (
+                <div className="listWrapper">
+                  <li className={`item${index}`}>
+                    <div className="item">
+                      {val.data} {index}
+                    </div>
+                    <DeleteForeverIcon
+                      onClick={() => dispatch(deleteItem(val.id))}
+                    />
+                  </li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
